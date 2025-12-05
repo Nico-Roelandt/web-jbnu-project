cc1bd4f755a1e2fab326a77067e9c512cfb08ad2
anne passelegue <annepas69@gmail.com>
2025-12-05 13:00:52 +0900

    Implementation of ranking controller

--- controller/rankingController.js
+++ controller/rankingController.js
@@ -1,11 +1,117 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db");
 
-exports.getRanking = (req, res) =>
-{
-    // Logic to get ranking
-    res.send("Get the ranking");
-}
+/**
+ * Get the ranking of users based on number of victories
+ */
+exports.getVictoriesRanking = (req, res) => {
+    const sql = `
+        SELECT id, username, nb_victories
+        FROM user
+        ORDER BY nb_victories DESC;
+    `;
 
-router.get("/", exports.getRanking);
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+/**
+ * Get the podium (top 3 users) based on number of victories
+ */
+exports.getVictoriesTop3 = (req, res) => {
+    const sql = `
+        SELECT id, username, nb_victories
+        FROM user
+        ORDER BY nb_victories DESC
+        LIMIT 3;
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+/**
+ * Get the ranking of users based on average attempt number
+ */
+exports.getAttemptsRanking = (req, res) => {
+    const sql = `
+        SELECT id, username, avg_attempts
+        FROM user
+        ORDER BY avg_attempts ASC;
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+/**
+ * Get the podium (top 3 users) based on average attempt number
+ */
+exports.getAttemptsTop3 = (req, res) => {
+    const sql = `
+        SELECT id, username, avg_attempts
+        FROM user
+        ORDER BY avg_attempts ASC
+        LIMIT 3;
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+/**
+ * Get the ranking of users based on number of games played
+ */
+exports.getGamesRanking = (req, res) => {
+    const sql = `
+        SELECT id, username, nb_games
+        FROM user
+        ORDER BY nb_games DESC;
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+/**
+ * Get the podium (top 3 users) based on number of games played
+ */
+exports.getGamesTop3 = (req, res) => {
+    const sql = `
+        SELECT id, username, nb_games
+        FROM user
+        ORDER BY nb_games DESC
+        LIMIT 3;
+    `;
+
+    pool.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+
+        res.json(results);
+    });
+};
+
+
+router.get("/victories", exports.getVictoriesRanking);
+router.get("/victories/top3", exports.getVictoriesTop3);
+router.get("/attempts", exports.getAttemptsRanking);
+router.get("/attempts/top3", exports.getAttemptsTop3);
+router.get("/games", exports.getGamesRanking);
+router.get("/games/top3", exports.getGamesTop3);
 module.exports = router;
